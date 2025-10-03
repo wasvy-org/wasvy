b92c48b81b548e54af6728adc5d70d7dfa799793
Marc Guiselin <marc@guiselin.com>
2025-10-04 00:10:16 +0200

    Improve example

--- examples/simple/src/lib.rs
+++ examples/simple/src/lib.rs
@@ -7,7 +7,7 @@ mod bindings {
         }
     });
 }
-use bevy_math::Vec3;
+use bevy_math::{Quat, Vec3};
 use bevy_transform::components::Transform;
 use bindings::{
     wasvy::ecs::app::{App, Query, QueryFor, Schedule, System},
@@ -20,63 +20,79 @@ struct GuestComponent;
 impl Guest for GuestComponent {
     fn setup() {
         // Define a new system that queries for entities with a Transform and a Marker component
-        let my_system = System::new("my-system");
-        my_system.add_commands();
-        my_system.add_query(&[
-            QueryFor::Mut("simple::MyStruct".to_string()),
-            QueryFor::Ref("bevy_transform::components::transform::Transform".to_string()),
+        let spin_cube = System::new("spin-cube");
+        spin_cube.add_query(&[
+            QueryFor::Mut("bevy_transform::components::transform::Transform".to_string()),
+            QueryFor::With("host_example::MyMarker".to_string()),
         ]);
 
-        // Register the system to run in the Update schedule
+        // Define another example system with commands
+        let my_system = System::new("my-system");
+        my_system.add_commands();
+        my_system.add_query(&[QueryFor::With("simple::MyStruct".to_string())]);
+
+        // Register the systems to run in the Update schedule
         let app = App::new();
-        app.add_systems(Schedule::Update, vec![my_system]);
+        app.add_systems(Schedule::Update, vec![my_system, spin_cube]);
     }
 
-    fn my_system(commands: Commands, query: Query) -> () {
-        println!("Running my-system");
-
-        let mut count = 0;
+    fn spin_cube(query: Query) {
         while let Some(components) = query.iter() {
-            count += 1;
+            // Get and deserialize the first component
+            let mut transform: Transform = from_json(&components[0].get());
 
-            let mut my_struct: MyStruct =
-                serde_json::from_str(&components[0].get()).expect("serializable component");
+            // Spin the cube
+            transform.rotate(Quat::from_rotation_y(0.025));
 
-            my_struct.value += 1;
-
-            let string = serde_json::to_string(&my_struct).expect("serializable component");
-            components[0].set(&string);
-
-            if count == 1 {
-                println!("Update my_struct {}", string);
-                println!("Read transform {}", components[1].get());
-            }
+            // Set the new component value
+            components[0].set(&to_json(&transform));
         }
-        println!("query entity count {count}");
+    }
+
+    fn my_system(commands: Commands, query: Query) {
+        // Count how many entities we've spawned
+        let mut count = 0;
+        while let Some(_) = query.iter() {
+            count += 1;
+        }
+
+        // Avoid spawning more than 10
+        if count > 10 {
+            return;
+        }
 
         #[derive(Serialize, Deserialize)]
         struct MyStruct {
             value: i32,
         }
 
-        if count < 10 {
-            let component_1 = MyStruct { value: 0 };
-            let component_2 = Transform::IDENTITY.looking_at(Vec3::ONE, Vec3::Y);
+        println!("Spawning an entity with MyStruct component in my-system");
 
-            let component_1_json =
-                serde_json::to_string(&component_1).expect("serializable component");
-            let component_2_json =
-                serde_json::to_string(&component_2).expect("serializable component");
+        let component_1 = MyStruct { value: 0 };
+        let component_2 = Transform::default().looking_at(Vec3::ONE, Vec3::Y);
 
-            commands.spawn(&[
-                ("simple::MyStruct".to_string(), component_1_json),
-                (
-                    "bevy_transform::components::transform::Transform".to_string(),
-                    component_2_json,
-                ),
-            ]);
-        }
+        commands.spawn(&[
+            ("simple::MyStruct".to_string(), to_json(&component_1)),
+            (
+                "bevy_transform::components::transform::Transform".to_string(),
+                to_json(&component_2),
+            ),
+        ]);
     }
 }
 
 export!(GuestComponent);
+
+fn from_json<'a, T>(component: &'a str) -> T
+where
+    T: Deserialize<'a>,
+{
+    serde_json::from_str(component).expect("serializable component")
+}
+
+fn to_json<T>(component: &T) -> String
+where
+    T: Serialize,
+{
+    serde_json::to_string(&component).expect("serializable component")
+}

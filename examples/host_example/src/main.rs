db942af9f4e94db63d0416b4fac114957b4cd9e1
Marc Guiselin <marc@guiselin.com>
2025-10-07 17:02:06 +0200

    Update to bevy 0.17.2 (#16)
    * Get world inspector working again
    
    * Update to bevy 1.17.2

--- examples/host_example/src/main.rs
+++ examples/host_example/src/main.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 use bevy::{DefaultPlugins, app::App};
+use bevy_egui::EguiPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 // Get started by importing the prelude
 use wasvy::prelude::*;
@@ -10,14 +12,22 @@ fn main() {
         .add_plugins((
             // Next, add the [`ModloaderPlugin`] ;)
             ModloaderPlugin::default(),
+            // Plus some helpers for the example
+            EguiPlugin::default(),
+            WorldInspectorPlugin::new(),
         ))
-        .add_systems(Startup, startup)
+        .add_systems(Startup, (load_mods, setup))
         .run();
 }
 
 /// Access the modloader's api through the Mods interface
-fn startup(mut mods: Mods) {
+fn load_mods(mut mods: Mods) {
     // Load one (or several) mods at once from the asset directory!
     mods.load("mods/simple.wasm");
     mods.load("mods/python.wasm");
+}
+
+fn setup(mut commands: Commands) {
+    // Having a camera in the scene is necessary for egui
+    commands.spawn(Camera3d::default());
 }
--- flake.nix
+++ flake.nix
@@ -28,41 +28,6 @@
             overlays = [ (import rust-overlay) ];
           };
           inherit (pkgs) lib;
-          # cargo-component nixos package is available, just stuck on 0.20.0
-          cargoComponent = pkgs.rustPlatform.buildRustPackage rec {
-            pname = "cargo-component";
-            version = "0.21.1";
-
-            src = pkgs.fetchFromGitHub {
-              owner = "bytecodealliance";
-              repo = "cargo-component";
-              rev = "v${version}";
-              hash = "sha256-Tlx14q/2k/0jZZ1nECX7zF/xNTeMCZg/fN+fhRM4uhc=";
-            };
-
-            useFetchCargoVendor = true;
-            cargoHash = "sha256-ZwxVhoqAzkaIgcH9GMR+IGkJ6IOQVtmt0qcDjdix6cU=";
-
-            nativeBuildInputs = [
-              pkgs.pkg-config
-            ];
-
-            buildInputs = [
-              pkgs.openssl
-            ];
-
-            # requires the wasm32-wasi target
-            doCheck = false;
-
-            meta = with lib; {
-              description = "Cargo subcommand for creating WebAssembly components based on the component model proposal";
-              homepage = "https://github.com/bytecodealliance/cargo-component";
-              changelog = "https://github.com/bytecodealliance/cargo-component/releases/tag/${src.rev}";
-              license = licenses.asl20;
-              maintainers = with maintainers; [ figsoda ];
-              mainProgram = "cargo-component";
-            };
-          };
           # TODO: submit wkg to nix-packages
           wkg = pkgs.rustPlatform.buildRustPackage rec {
             pname = "wkg";
@@ -75,7 +40,6 @@
               hash = "sha256-VZ+rUZi6o2onMFxK/BMyi6ZjuDS0taJh5w3r33KCZTU=";
             };
             
-            useFetchCargoVendor = true;
             cargoHash = "sha256-dHhJT/edEYagLQoUcXCLPA4fUJdN9ZoOITLpWAH5p/0=";
 
             nativeBuildInputs = [ pkgs.pkg-config ];
@@ -103,7 +67,6 @@
           buildInputs = with pkgs; [
             # Dev tools
             just
-            cargoComponent
             wkg
 
             # Build tools

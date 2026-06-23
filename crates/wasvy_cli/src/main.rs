efc734795cf06d60d8df7552374877fd04a749a7
Marc Guiselin <marc@guiselin.com>
2026-06-23 16:40:23 +0200

    Bump bevy

--- crates/wasvy_cli/src/main.rs
+++ crates/wasvy_cli/src/main.rs
@@ -11,7 +11,7 @@ use wasvy_cli::{
 
 pub fn main() {
     let version = env!("CARGO_PKG_VERSION");
-    let bevy = "0.18.0"; // Updated by ci
+    let bevy = "0.19.0"; // Updated by ci
     println!("Wasvy CLI v{version} for Bevy v{bevy}");
     println!();
     let args = Args::parse();

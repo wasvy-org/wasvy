019c03aef89916e14cbd07b0f6934a4e813379e6
Marc Guiselin <marc@guiselin.com>
2026-06-24 08:43:23 +0200

    Fix flaky tests

--- crates/wasvy_mock/src/lib.rs
+++ crates/wasvy_mock/src/lib.rs
@@ -141,7 +141,7 @@ impl MockApp {
         });
 
         ready_receiver
-            .recv_timeout(Duration::from_millis(50))
+            .recv_timeout(Duration::from_secs(1))
             .expect("Host app ready");
 
         Mock {
